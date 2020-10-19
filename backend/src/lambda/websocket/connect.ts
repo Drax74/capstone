a357760e3df775a218653de0741cdeba11904ec6
Drax74 <draksic.m@gmail.com>
2020-10-19 22:05:29 +0200

    Websockets setup

--- backend/src/lambda/websocket/connect.ts
+++ backend/src/lambda/websocket/connect.ts
@@ -1,28 +1,36 @@
-import { APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import {
+  APIGatewayProxyHandler,
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult
+} from 'aws-lambda'
 import 'source-map-support/register'
-import * as AWS  from 'aws-sdk'
+import * as AWS from 'aws-sdk'
 
 const docClient = new AWS.DynamoDB.DocumentClient()
 
 const connectionsTable = process.env.CONNECTIONS_TABLE
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+export const handler: APIGatewayProxyHandler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
   console.log('Websocket connect', event)
 
   const connectionId = event.requestContext.connectionId
   const timestamp = new Date().toISOString()
 
   const item = {
-    id: connectionId,
-    timestamp
+    userId: timestamp,
+    id: connectionId
   }
 
   console.log('Storing item: ', item)
 
-  await docClient.put({
-    TableName: connectionsTable,
-    Item: item
-  }).promise()
+  await docClient
+    .put({
+      TableName: connectionsTable,
+      Item: item
+    })
+    .promise()
 
   return {
     statusCode: 200,
