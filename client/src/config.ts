e9d8aa21ee4105ab38ca9ec3ee002f2f1df2241a
Drax74 <draksic.m@gmail.com>
2020-10-21 04:38:32 +0200

    Cleaning up TODOS

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,9 +1,7 @@
-// TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
 const apiId = 'zcbu5bwcd2'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
-  // TODO: Create an Auth0 application and copy values from it into this map
   domain: 'dev-35u5rsu8.eu.auth0.com', // Auth0 domain
   clientId: '14hQNHpUQ9Hp26SpPisb8Oinj3Q8xYDr', // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
